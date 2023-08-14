31501230063d1eb5296ab328c20f222b4e22645f
Jimmy Teran <jim95.teran@gmail.com>
2023-08-14 14:19:29 -0400

    Updating grants score when monitoring reviews is executed

--- functions/constants/collections.js
+++ functions/constants/collections.js
@@ -1,7 +1,7 @@
 module.exports = {
   MINTED_BLOCK_COLLECTION: 'mintEventBlock',
   FORMS_COLLECTION: 'reviewForms',
-  REQUESTS_COLLECTION: 'reviewRequests',
   REVIEWS_COLLECTION: 'reviews',
+  REVIEW_REQUESTS_COLLECTION: 'reviewRequests',
   GRANTS_COLLECTION: 'grants',
 }
--- functions/services/DeresyDBService.js
+++ functions/services/DeresyDBService.js
@@ -2,12 +2,14 @@ const { db } = require('../firebase')
 
 const {
   FORMS_COLLECTION,
-  REQUESTS_COLLECTION,
+  REVIEW_REQUESTS_COLLECTION,
   REVIEWS_COLLECTION,
+  GRANTS_COLLECTION,
 } = require('../constants/collections')
 const formsRef = db.collection(FORMS_COLLECTION)
-const requestsRef = db.collection(REQUESTS_COLLECTION)
+const reviewRequestsRef = db.collection(REVIEW_REQUESTS_COLLECTION)
 const reviewsRef = db.collection(REVIEWS_COLLECTION)
+const grantsRef = db.collection(GRANTS_COLLECTION)
 
 const saveForm = async (formID, data) => {
   const snapshot = await formsRef
@@ -28,17 +30,17 @@ const saveForm = async (formID, data) => {
 }
 
 const saveRequest = async (requestName, data) => {
-  const snapshot = await requestsRef
+  const snapshot = await reviewRequestsRef
     .where('requestName', '==', requestName)
     .limit(1)
     .get()
 
   if (snapshot.empty) {
-    await requestsRef.add({
+    await reviewRequestsRef.add({
       ...data,
     })
   } else {
-    const document = requestsRef.doc(snapshot.docs[0].id)
+    const document = reviewRequestsRef.doc(snapshot.docs[0].id)
     await document.update({
       ...data,
     })
@@ -63,8 +65,41 @@ const saveReviews = async (requestName, data) => {
   }
 }
 
+const updateGrant = async (id, payload) => {
+  await grantsRef.doc(id).update({ ...payload })
+}
+
+const getGrantByTarget = async requestTarget => {
+  const snapshot = await grantsRef
+    .where('request_target', '==', requestTarget)
+    .limit(1)
+    .get()
+
+  return { documentId: snapshot.docs[0].id, ...snapshot.docs[0].data() }
+}
+
+const getReviewRequest = async requestName => {
+  const snapshot = await reviewRequestsRef
+    .where('requestName', '==', requestName)
+    .limit(1)
+    .get()
+  return snapshot.docs[0].data()
+}
+
+const getReviews = async requestName => {
+  const snapshot = await reviewsRef
+    .where('requestName', '==', requestName)
+    .limit(1)
+    .get()
+  return snapshot.docs[0].data()
+}
+
 module.exports = {
+  getGrantByTarget,
+  getReviews,
+  getReviewRequest,
   saveForm,
   saveRequest,
   saveReviews,
+  updateGrant,
 }
